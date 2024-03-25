4131285982373e9aa3b693bdccc56e9f3fab9293
Stefano <stef.pro@hotmail.com>
2024-03-25 14:52:03 -0400

    fix: add email property to User class

--- app/src/main/java/models/User.java
+++ app/src/main/java/models/User.java
@@ -8,6 +8,7 @@ public class User {
     private int userId;
     private String firstName;
     private String lastName;
+    private String email;
     private String userName;
     private String password;
     private ArrayList<Integer> listingIds;
@@ -16,10 +17,11 @@ public class User {
     public User() {
     }
 
-    public User(int userId, String firstName, String lastName, String userName, String password, ArrayList<Integer> listingIds, ArrayList<Integer> transactionIds) {
+    public User(int userId, String firstName, String lastName, String email, String userName, String password, ArrayList<Integer> listingIds, ArrayList<Integer> transactionIds) {
         this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
         this.userName = userName;
         this.password = password;
         this.listingIds = listingIds;
@@ -49,6 +51,10 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
 
     public String getUserName() {
         return userName;
