62602f650a6c6084b951990977e95666375e049f
Igor Maia <igor.ribas15@gmail.com>
2024-03-28 16:55:13 -0400

    chore: add files for homescreen

---
+++ app/src/main/java/com/example/spot_seeker/HomeScreen.java
@@ -0,0 +1,31 @@
+package com.example.spot_seeker;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        final DrawerLayout drawerLayout =findViewById(R.id.drawerLayout);
+
+        findViewById(R.id.menuImage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+
+    }
+}
