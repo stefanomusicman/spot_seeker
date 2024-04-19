d24c044f24b985cb3991a997934d5828f5acf33f
Stefano <stef.pro@hotmail.com>
2024-04-19 14:29:36 -0400

    chore: create async method to convert list of listing id to list of listing objects

--- app/src/main/java/com/example/spot_seeker/MySpots.java
+++ app/src/main/java/com/example/spot_seeker/MySpots.java
@@ -2,9 +2,16 @@ package com.example.spot_seeker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+
+import java.util.ArrayList;
+
+import models.Listing;
+import models.ListingsCallback;
+import models.UserSingleton;
 
 public class MySpots extends AppCompatActivity {
 
@@ -18,6 +25,15 @@ public class MySpots extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MySpots.this, DetailedSpot.class));
+            }
+        });
+
+        UserSingleton.getUserListings(new ListingsCallback() {
+            @Override
+            public void onListingsLoaded(ArrayList<Listing> listings) {
+                // Handle the retrieved listings here
+                Log.d("USER LISTINGS", listings.toString());
+                // Do whatever you need with the listings, such as updating UI or processing data
             }
         });
     }
