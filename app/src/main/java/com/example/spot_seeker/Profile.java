04988e9c606a60f474bae8411cb508fd2e4f285e
Stefano <stef.pro@hotmail.com>
2024-04-24 15:12:50 -0400

    chore: basic logic setup for user profile update

--- app/src/main/java/com/example/spot_seeker/Profile.java
+++ app/src/main/java/com/example/spot_seeker/Profile.java
@@ -1,49 +1,66 @@
 package com.example.spot_seeker;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import models.UserSingleton;
+
 public class Profile extends AppCompatActivity {
 
-    private TextView emailTextView, phoneTextView, addressTextView;
-    private EditText updateEmail, updatePhone, updateAddress;
-    private Button updateInformation;
+    private TextView tvFullName, tvUserId;
+    private EditText edFName, edLName, edUsername;
+    private Button btnUpdate;
+    private DatabaseReference userDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        initialize();
+    }
 
-        // Initialize UI elements
-        emailTextView = findViewById(R.id.emailTextView);
-        phoneTextView = findViewById(R.id.phoneTextView);
-        addressTextView = findViewById(R.id.addressTextView);
-        updateEmail = findViewById(R.id.updateEmail);
-        updatePhone = findViewById(R.id.updatePhone);
-        updateAddress = findViewById(R.id.updateAddress);
-        updateInformation = findViewById(R.id.Updateinformation);
+    private void initialize() {
+        // Initialize widgets
+        tvFullName = findViewById(R.id.tvFullName);
+        tvUserId = findViewById(R.id.tvUserId);
+        edFName = findViewById(R.id.edFName);
+        edLName = findViewById(R.id.edLName);
+        edUsername = findViewById(R.id.edUsername);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        // Initialize Database Reference
+        userDatabase = FirebaseDatabase.getInstance().getReference("Users");
+        // Print User's full name and user id in header section beneath the user icon
+        String fullName = UserSingleton.getUser().getFirstName() + " " + UserSingleton.getUser().getLastName();
+        String userId = String.valueOf(UserSingleton.getUser().getUserId());
+        tvFullName.setText(fullName);
+        tvUserId.setText(userId);
 
-        // Set onClickListener for the updateInformation button
-        updateInformation.setOnClickListener(view -> {
-            // Handle the update information logic here
-            // You can retrieve the new email, phone, and address from the corresponding EditText fields
-            String newEmail = updateEmail.getText().toString().trim();
-            String newPhone = updatePhone.getText().toString().trim();
-            String newAddress = updateAddress.getText().toString().trim();
-
-            // Update the TextViews with the new information
-            emailTextView.setText(newEmail);
-            phoneTextView.setText(newPhone);
-            addressTextView.setText(newAddress);
-
-            // Clear the EditText fields
-            updateEmail.setText("");
-            updatePhone.setText("");
-            updateAddress.setText("");
+        // Set onClick listener for the update button
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateUser();
+            }
         });
+    }
+
+    private void updateUser() {
+        if(!edFName.getText().toString().trim().isEmpty()) {
+            UserSingleton.getUser().setFirstName(edFName.getText().toString().trim());
+        }
+        if(!edLName.getText().toString().trim().isEmpty()) {
+            UserSingleton.getUser().setLastName(edFName.getText().toString().trim());
+        }
+        if(!edUsername.getText().toString().trim().isEmpty()) {
+            UserSingleton.getUser().setUserName(edUsername.getText().toString().trim());
+        }
     }
 }
