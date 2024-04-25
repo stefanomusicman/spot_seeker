a1f7cda52bb79f21463a9b9f055cb27b6ce43cc9
Daniel <venturole.daniel@gmail.com>
2024-04-25 00:39:36 -0400

    <maps>

--- app/src/main/java/com/example/spot_seeker/MainActivity.java
+++ app/src/main/java/com/example/spot_seeker/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.spot_seeker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
+            GoogleMap.OnMarkerClickListener
+    {
+        private GoogleMap spotMap;
+
+        private Marker markerHome;
+
+        private final LatLng spot1 = new LatLng(45.508164, -73.574621);
+        @Override
+        public void onMapReady(@NonNull GoogleMap googleMap) {
+            spotMap = googleMap;
+
+            markerHome = spotMap.addMarker(new MarkerOptions()
+                    .position(spot1)
+                    .title("Spot1"));
+            markerHome.setTag(0);
+            spotMap.moveCamera(CameraUpdateFactory.newLatLngZoom(spot1, 12));
+
+            googleMap.setOnMarkerClickListener(this);
+
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.activity_home_screen);
+
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+            mapFragment.getMapAsync(this);
+        }
+
+        @Override
+        public boolean onMarkerClick(@NonNull Marker marker) {
+            return false;
+        }
     }
 }
